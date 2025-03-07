1ab37b7d2a1c285778bf8fab7788b97e09bf7f90
Amit Singh4 <amit_singh4@epam.com>
2025-03-07 12:26:42 +0530

    updated the url config

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,21 +1,21 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const path = event.rawPath;
-    const method = event.requestContext.http.method;
+    const path = event.rawPath || event.path;
+    const method = event.requestContext?.http?.method || event.httpMethod;
 
     if (path === "/hello" && method === "GET") {
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: "Hello from Lambda" }),
-            headers: { "Content-type": "application/json" }
-        }
-    } else {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
-            }),
-            headers: { "Content-Type": "application/json" }
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ statusCode: 200, message: "Hello from Lambda" }),
         };
     }
-};
+
+    return {
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            "statusCode": 400,
+            "message": `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        }),
+    };
+}
