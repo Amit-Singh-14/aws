8cf3241fc148c0795bda5ba2d13082be32f47b3e
Amit Singh4 <amit_singh4@epam.com>
2025-03-11 20:41:00 +0530

    comeplted task04

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,9 +1,4 @@
 exports.handler = async (event) => {
-    console.log("Received SNS Event:", JSON.stringify(event, null, 2));
-
-    for (const record of event.Records) {
-        console.log("SNS Message:", record.Sns.Message);
-    }
-
-    return { statusCode: 200, body: "Processed SNS Messages" };
+    console.log("SNS Event Received:", JSON.stringify(event, null, 2));
+    return { statusCode: 200, body: "Message logged successfully." };
 };
--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,9 +1,4 @@
 exports.handler = async (event) => {
-    console.log("Received SQS Event:", JSON.stringify(event, null, 2));
-
-    for (const record of event.Records) {
-        console.log("Message Body:", record.body);
-    }
-
-    return { statusCode: 200, body: "Processed SQS Messages" };
+    console.log("SQS Event Received:", JSON.stringify(event, null, 2));
+    return { statusCode: 200, body: "Message logged successfully." };
 };
